a0092c34af877b7fd6939d762466ea08f57945bb
kim-jong-young <kim-jong-young@218.38.137.27>
2022-12-17 20:40:57 +0900

    Eureka Client 설정 추가

--- board-config-server/src/main/java/edu/jong/board/config/ConfigServerApplication.java
+++ board-config-server/src/main/java/edu/jong/board/config/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package edu.jong.board.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
