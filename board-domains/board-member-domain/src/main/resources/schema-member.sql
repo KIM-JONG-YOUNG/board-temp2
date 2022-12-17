382e0c0af7e331fb546fd661599582a05b572fc0
kim-jong-young <kim-jong-young@218.38.137.27>
2022-12-17 21:52:53 +0900

    테이블 스키마 쿼리 수정

--- board-domains/board-member-domain/src/main/resources/schema-member.sql
+++ board-domains/board-member-domain/src/main/resources/schema-member.sql
@@ -1,13 +1,13 @@
 create table if not exists tb_member (
-   member_no bigint not null auto_increment,
-    created_date_time datetime(6),
-    state integer,
-    updated_date_time datetime(6),
+   	member_no bigint not null auto_increment,
     member_email varchar(60) not null,
     member_gender char(1) not null,
     member_name varchar(30) not null,
     member_password varchar(60) not null,
     member_username varchar(30) not null,
+    created_date_time datetime(6),
+    updated_date_time datetime(6),
+    state integer,
     primary key (member_no),
     unique (member_username)
 );
