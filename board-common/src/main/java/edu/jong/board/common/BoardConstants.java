b5e629425731e54c0956fb513bc13116b075a0dc
kim-jong-young <kim-jong-young@218.38.137.27>
2022-12-18 01:39:08 +0900

    테이블명 수정

--- board-common/src/main/java/edu/jong/board/common/BoardConstants.java
+++ board-common/src/main/java/edu/jong/board/common/BoardConstants.java
@@ -10,7 +10,7 @@ public final class BoardConstants {
 		public static final String TB_ROLE = "tb_role";
 		public static final String TB_AUTH = "tb_authority";
 		public static final String TB_ROLE_AUTH = "tb_role_authority";
-		public static final String TB_ROLE_MEMBER = "tb_role_member";
+		public static final String TB_MEMBER_ROLE = "tb_member_role";
 		public static final String TB_POST = "tb_post";
 	}
 }
